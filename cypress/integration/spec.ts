c89729a0c529780d154b5ed40a195b0e284012a7
samkelozuma66 <samkelozuma66@gmail.com>
2021-11-30 20:12:17 +0200

    Adding Testing Package

---
+++ cypress/integration/spec.ts
@@ -0,0 +1,9 @@
+describe('My First Test', () => {
+  it('Visits the initial project page', () => {
+    cy.visit('/')
+    cy.contains('key')
+    cy.get('[id=key]').type('1d9fb201fbd3349e6549e776f210d9fc')
+    cy.get('[id=token]').type('65b4737cef3499ab25e684370c65313b462aef447bd280b358c97f5dfb56db63')
+    cy.get('button').trigger('click')
+  })
+})
